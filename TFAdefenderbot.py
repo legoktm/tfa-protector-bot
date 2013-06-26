3026f2a55005a168b9f6515d0200acbadda10ebf
Kunal Mehta <legoktm@gmail.com>
2013-06-26 00:55:01 -0700

    refactor should_we_protect to return what to protect, rather than a boolean. implement full protection of redirects

--- TFAdefenderbot.py
+++ TFAdefenderbot.py
@@ -17,40 +17,49 @@ enwp = pywikibot.Site('en', 'wikipedia')
 token = enwp.token(pywikibot.Page(enwp, 'Main Page'), 'protect')
 
 
-def should_we_protect(p_status, tmrw):
-    if not p_status:
-        print 'Unprotected. Will protect.'
-        return True
+def should_we_protect(p_status, tmrw, redirect=False):
+    #redirect is true if we should also check for edit protection.
+    #will return a dict of {'type':{'level':'sysop','expiry':ts}}
+    return_this = {}
     move = p_status.get('move', None)
+    if move:
+        if move['level'] != 'sysop':
+            return_this['move'] = {'level': 'sysop', 'expiry': tmrw}
+        elif move['expiry'] != 'infinity':
+            ts = pywikibot.Timestamp.fromISOformat(move['expiry'])
+            if ts < tmrw:
+                #expires before off main page.
+                return_this['move'] = {'level': 'sysop', 'expiry': tmrw}
+    else:
+        return_this['move'] = {'level': 'sysop', 'expiry': tmrw}
     edit = p_status.get('edit', None)
-    if not move:
-        print 'Not move protected. Will protect.'
-        return True
-    if move['level'] != 'sysop':
-        print 'Not sysop-protected.'
-        return True
-    if move['expiry'] == 'infinity':
-        print 'Indefinitely protect. Will skip.'
-        return False
-    ts = pywikibot.Timestamp.fromISOformat(move['expiry'])
-    if ts < tmrw:
-        print 'Expires before it is off the main page. Will protect'
-        return True
-    print 'Looks good to me. +2'
-    return False
+    if redirect:
+        if edit:
+            if edit['level'] != 'sysop':
+                return_this['edit'] = {'level': 'sysop', 'expiry': tmrw}
+            elif edit['expiry'] != 'infinity':
+                ts = pywikibot.Timestamp.fromISOformat(edit['expiry'])
+                if ts < tmrw:
+                    return_this['edit'] = {'level': 'sysop', 'expiry': tmrw}
+        else:
+            return_this['edit'] = {'level': 'sysop', 'expiry': tmrw}
+    return return_this
 
 
-def protect(page, tmrw, p_status):
-    expiry = tmrw.strftime("%Y-%m-%dT%H:%M:%SZ")
+def protect(page, p_status, protect_this):
     params = {'action': 'protect',
               'title': page.title(),
               'token': token,
-              'protections': 'move=sysop',
-              'expiry': expiry,
+              'protections': [],  # pwb will convert these to a string later on
+              'expiry': [],
               'reason': 'Upcoming TFA ([[WP:BOT|bot protection]])',
               }
+    for p_type in protect_this:
+        params['protections'].append('{0}={1}'.format(p_type, protect_this[p_type]['level']))
+        params['expiry'].append(protect_this[p_type]['expiry'].strftime("%Y-%m-%dT%H:%M:%SZ"))
     for p_type in p_status:
-        if p_type == 'move':
+        if p_type in protect_this:
+            #dont try to protect what we want to change
             continue
         if 'source' in p_status[p_type]:
             #skip cascading protection
@@ -91,14 +100,21 @@ def do_page(date):
     if not title:
         return None
     tfa = pywikibot.Page(enwp, title)
+    p_status = prot_status(tfa)
     if tfa.isRedirectPage():
-        #do something
-        pass
+        real_tfa = tfa.getRedirectTarget()
+        real_p_status = prot_status(real_tfa)
+        protect_this_redirect = should_we_protect(p_status, d_plus_one, redirect=True)
+        if protect_this_redirect:
+            protect(tfa, p_status, protect_this_redirect)
+        real_protect_this = should_we_protect(p_status, d_plus_one)
+        if real_protect_this:
+            protect(real_tfa, real_p_status, real_protect_this)
         return True
     else:
-        p_status = prot_status(tfa)
-        if should_we_protect(p_status, d_plus_one):
-            protect(tfa, d_plus_one, p_status)
+        protect_this = should_we_protect(p_status, d_plus_one)
+        if protect_this:
+            protect(tfa, d_plus_one, protect_this)
             return True
 
 
