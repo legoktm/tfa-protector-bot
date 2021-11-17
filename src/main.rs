a2ab8267b1f9c1c17ad10c2d29a149e735cc9e14
Kunal Mehta <legoktm@debian.org>
2021-11-16 22:53:47 -0800

    Port to use mwbot

--- src/main.rs
+++ src/main.rs
@@ -15,116 +15,33 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 use anyhow::{anyhow, Result};
-use chrono::prelude::*;
-use chrono::Duration;
+use chrono::{prelude::*, Duration};
 use log::{debug, error, info};
-use mediawiki::{
-    api::Api,
-    page::{Page, PageError},
-    title::Title,
-};
-use serde::Deserialize;
-use std::collections::HashMap;
+use mwapi_responses::{prelude::*, protection::ProtectionInfo};
+use mwbot::{Bot, Error};
 
-const USER_AGENT: &str =
-    "https://en.wikipedia.org/wiki/User:TFA_Protector_Bot tfaprotbot-rs";
-
-/// Login information, stored in auth.toml
-#[derive(Deserialize)]
-struct Auth {
-    username: String,
-    password: String,
-}
-
-async fn mwapi() -> Result<Api> {
-    let mut api = Api::new("https://en.wikipedia.org/w/api.php").await?;
-    api.set_user_agent(USER_AGENT);
-    api.set_maxlag(Some(999999999)); // Don't worry about it
-    Ok(api)
-}
-
-/// Get a logged-in `Api` instance
-async fn mwapi_authed() -> Result<Api> {
-    let mut api = mwapi().await?;
-    let path = {
-        let first = std::path::Path::new("auth.toml");
-        if first.exists() {
-            first.to_path_buf()
-        } else {
-            dirs_next::home_dir()
-                .ok_or_else(|| anyhow!("Cannot find home directory"))?
-                .join("auth.toml")
-        }
-    };
-    debug!("Reading credentials from {:?}", path);
-    let auth: Auth = toml::from_str(&std::fs::read_to_string(path)?)?;
-    info!("Logging in as {}", auth.username);
-    api.login(auth.username, auth.password).await?;
-    Ok(api)
-}
-
-/// A protection entry
-#[derive(Deserialize, Debug)]
-struct Protection {
-    #[serde(rename = "type")]
-    type_: String,
-    level: String,
-    expiry: String,
-    #[serde(default)]
-    cascade: bool,
-    /// Source the cascade protection is coming from
-    source: Option<String>,
-}
+#[query(prop = "info", inprop = "protection")]
+struct ProtectionResponse;
 
 /// Query the current protection status of a page
-async fn protection_status(name: &str, api: &Api) -> Result<Vec<Protection>> {
-    let params = [
-        ("action", "query"),
-        ("prop", "info"),
-        ("inprop", "protection"),
-        ("titles", name),
-        ("formatversion", "2"),
-    ]
-    .iter()
-    .map(|&(k, v)| (k.to_string(), v.to_string()))
-    .collect();
-    let result = api.get_query_api_json(&params).await?;
-    let status: Vec<Protection> = serde_json::from_value(
-        result["query"]["pages"][0]["protection"].clone(),
-    )?;
-    Ok(status)
-}
-
-/// Get the page this one redirects to, if it's a redirect
-/// TODO: upstream this to mediawiki crate
-async fn get_redirect_target(name: &str, api: &Api) -> Result<Option<String>> {
-    let params = [
-        ("action", "query"),
-        ("redirects", "1"),
-        ("titles", name),
-        ("formatversion", "2"),
-    ]
-    .iter()
-    .map(|&(k, v)| (k.to_string(), v.to_string()))
-    .collect();
-    let result = api.get_query_api_json(&params).await?;
-    match result["query"].get("redirects") {
-        Some(redirects) => {
-            Ok(redirects[0]["to"].as_str().map(|s| s.to_string()))
-        }
-        // Not a redirect
-        None => Ok(None),
-    }
+async fn protection_status(
+    name: &str,
+    bot: &Bot,
+) -> Result<Vec<ProtectionInfo>> {
+    let mut params = ProtectionResponse::params().to_vec();
+    params.push(("titles", name));
+    let resp: ProtectionResponse = bot.get_api().get(&params).await?;
+    Ok(resp.query.pages[0].protection.clone())
 }
 
 async fn handle_page(
     name: &str,
     day: Date<Utc>,
-    api: &mut Api,
+    bot: &Bot,
     // Whether we also need to apply edit protection
     redirect: bool,
 ) -> Result<()> {
-    let status = protection_status(name, api).await?;
+    let status = protection_status(name, bot).await?;
     // dbg!(&status);
     // let title = Title::new_from_full(name, api);
     // Protect for a day
@@ -204,46 +121,34 @@ async fn handle_page(
         expiry.push(prot.expiry.clone());
     }
 
-    let mut params: HashMap<String, String> = [
+    let protections = protections.join("|");
+    let expiry = expiry.join("|");
+    let mut params = vec![
         ("action", "protect"),
-        ("title", &name),
-        ("protections", &protections.join("|")),
-        ("expiry", &expiry.join("|")),
+        ("title", name),
+        ("protections", &protections),
+        ("expiry", &expiry),
         ("reason", "Upcoming TFA ([[WP:BOT|bot protection]])"),
-        ("formatversion", "2"),
-        ("token", &api.get_edit_token().await?),
-    ]
-    .iter()
-    .map(|&(k, v)| (k.to_string(), v.to_string()))
-    .collect();
+    ];
     if cascade {
-        params.insert("cascade".to_string(), "1".to_string());
+        params.push(("cascade", "1"));
     }
-    let result = api.post_query_api_json(&params).await?;
-    match result.get("error") {
-        Some(errors) => Err(anyhow!(serde_json::to_string(errors)?)),
-        None => {
-            info!("Successfully protected {}", &name);
-            Ok(())
-        }
-    }
+    bot.get_api().post_with_token("csrf", &params).await?;
+    info!("Successfully protected {}", &name);
+    Ok(())
 }
 
-async fn get_tfa_title(day: Date<Utc>, api: &Api) -> Result<String> {
+async fn get_tfa_title(day: Date<Utc>, bot: &Bot) -> Result<String> {
     // First see if we can get it from Template:TFA title
-    let tfa_title = Title::new(
-        &format!("TFA title/{}", day.format("%B %-d, %Y").to_string()),
-        10,
-    );
-    let tfa_page = Page::new(tfa_title);
-    match tfa_page.text(&api).await {
+    let tfa_page = bot.get_page(&format!(
+        "Template:TFA title/{}",
+        day.format("%B %-d, %Y").to_string()
+    ));
+    match tfa_page.get_wikitext().await {
         Ok(text) => return Ok(text),
         // Nothing, keep trying
-        Err(PageError::Missing(title)) => {
-            debug!(
-                "{} didn't exist, will check TFA now",
-                title.full_pretty(&api).unwrap()
-            )
+        Err(Error::PageDoesNotExist(_)) => {
+            debug!("{} didn't exist, will check TFA now", tfa_page.title())
         }
         Err(e) => {
             return Err(anyhow!(e.to_string()));
@@ -251,19 +156,17 @@ async fn get_tfa_title(day: Date<Utc>, api: &Api) -> Result<String> {
     };
 
     // Try harder, parse it out of the TFA page itself
-    extract_tfa_title(day).await
+    extract_tfa_title(day, bot).await
 }
 
-async fn extract_tfa_title(day: Date<Utc>) -> Result<String> {
-    use parsoid::prelude::*;
+async fn extract_tfa_title(day: Date<Utc>, bot: &Bot) -> Result<String> {
+    use mwbot::parsoid::*;
 
-    let page = format!(
+    let page = bot.get_page(&format!(
         "Wikipedia:Today's featured article/{}",
         day.format("%B %-d, %Y").to_string()
-    );
-    let client =
-        Client::new("https://en.wikipedia.org/api/rest_v1", USER_AGENT)?;
-    let code = client.get(&page).await?;
+    ));
+    let code = page.get_html().await?;
     // Return the first bolded link
     for bold in code.select("b").iter() {
         // unwrap: We know that <b> tags turn into generic nodes
@@ -272,7 +175,7 @@ async fn extract_tfa_title(day: Date<Utc>) -> Result<String> {
             return Ok(links[0].target());
         }
     }
-    Err(anyhow!("could not find title for {}", page))
+    Err(anyhow!("could not find title for {}", page.title()))
 }
 
 #[tokio::main]
@@ -303,10 +206,10 @@ async fn main() {
 
 async fn run() -> Result<()> {
     let now = Utc::today();
-    let mut api = mwapi_authed().await?;
+    let bot = Bot::from_default_config().await?;
     for ahead in 1..35 {
         let day = now + Duration::days(ahead);
-        let text = match get_tfa_title(day, &api).await {
+        let text = match get_tfa_title(day, &bot).await {
             Ok(text) => text,
             Err(e) => {
                 debug!("{}", e.to_string());
@@ -317,14 +220,15 @@ async fn run() -> Result<()> {
                 continue;
             }
         };
-        match get_redirect_target(&text, &api).await? {
+        let page = bot.get_page(&text);
+        match page.get_redirect_target().await? {
             Some(target) => {
-                info!("{} redirects to {}", &text, &target);
-                handle_page(&text, day, &mut api, true).await?;
-                handle_page(&target, day, &mut api, false).await?;
+                info!("{} redirects to {}", &text, &target.title());
+                handle_page(&text, day, &bot, true).await?;
+                handle_page(target.title(), day, &bot, false).await?;
             }
             None => {
-                handle_page(&text, day, &mut api, false).await?;
+                handle_page(&text, day, &bot, false).await?;
             }
         }
     }
@@ -335,26 +239,21 @@ async fn run() -> Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::Path;
 
-    #[tokio::test]
-    async fn test_get_redirect_target() {
-        let api = mwapi().await.unwrap();
-        assert_eq!(
-            Some("Main Page".to_string()),
-            get_redirect_target("Main page", &api).await.unwrap()
-        );
-        assert_eq!(None, get_redirect_target("Main Page", &api).await.unwrap());
+    async fn test_bot() -> Bot {
+        Bot::from_path(Path::new("mwbot-test.toml")).await.unwrap()
     }
 
     #[tokio::test]
     async fn test_protection_status() {
-        let api = mwapi().await.unwrap();
-        assert!(protection_status("User:Legoktm/test", &api)
+        let bot = test_bot().await;
+        assert!(protection_status("User:Legoktm/test", &bot)
             .await
             .unwrap()
             .is_empty());
         let einstein =
-            protection_status("Albert Einstein", &api).await.unwrap();
+            protection_status("Albert Einstein", &bot).await.unwrap();
         assert_eq!(einstein[0].type_, "edit".to_string());
         assert_eq!(einstein[0].level, "autoconfirmed".to_string());
         assert_eq!(einstein[0].expiry, "infinity".to_string());
@@ -365,24 +264,29 @@ mod tests {
 
     #[tokio::test]
     async fn test_extract_tfa_title() {
+        let bot = test_bot().await;
         assert_eq!(
             "Zoo TV Tour".to_string(),
-            extract_tfa_title(Utc.ymd(2020, 02, 29)).await.unwrap()
+            extract_tfa_title(Utc.ymd(2020, 02, 29), &bot)
+                .await
+                .unwrap()
         );
         // Case normalization
         assert_eq!(
             "Mosaics of Delos".to_string(),
-            extract_tfa_title(Utc.ymd(2020, 02, 5)).await.unwrap()
+            extract_tfa_title(Utc.ymd(2020, 02, 5), &bot).await.unwrap()
         );
         // Unicode (was broken in Python)
         assert_eq!(
             "SMS Zähringen".to_string(),
-            extract_tfa_title(Utc.ymd(2020, 02, 6)).await.unwrap()
+            extract_tfa_title(Utc.ymd(2020, 02, 6), &bot).await.unwrap()
         );
         // Italics
         assert_eq!(
             "The Cabinet of Dr. Caligari".to_string(),
-            extract_tfa_title(Utc.ymd(2020, 02, 26)).await.unwrap()
+            extract_tfa_title(Utc.ymd(2020, 02, 26), &bot)
+                .await
+                .unwrap()
         );
     }
 }
